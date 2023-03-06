6175eb55fc463663df1c21261f52bf664a8c69b5
dmitrylobov <dilo1992.edu@gmail.com>
2023-03-06 19:45:21 +0300

    init commit

---
+++ src/main/java/by/lobov/cicdexamplecw57/CicdExampleCw57Application.java
@@ -0,0 +1,13 @@
+package by.lobov.cicdexamplecw57;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdExampleCw57Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdExampleCw57Application.class, args);
+	}
+
+}
---
+++ src/test/java/by/lobov/cicdexamplecw57/CicdExampleCw57ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lobov.cicdexamplecw57;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdExampleCw57ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
